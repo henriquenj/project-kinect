2c79a3c87b84d6a2d09bfbc6f52d5cbe43ad17a3
henriquenj@gmail.com <henriquenj@gmail.com@fb52f868-46a9-6944-04f2-c40b472b6950>
2013-08-05 05:54:41 +0000

    * Config SubWCRev tool * A little bit of work on the interface

--- MainFrame.cpp
+++ MainFrame.cpp
@@ -1,12 +1,54 @@
 #include "MainFrame.h"
 
 MainFrame::MainFrame(const wxString& title, const wxPoint& pos, const wxSize& size)
-	:wxFrame(NULL, -1, title, pos, size)
+	:wxFrame(NULL, wxID_ANY, title, pos, size)
 {
+	CreateStatusBar();
 
+	// help menu
+	wxMenu* helpMenu = new wxMenu;
+	this->Connect( K_About,  wxEVT_COMMAND_MENU_SELECTED, wxCommandEventHandler(MainFrame::OnAbout));
+	helpMenu->Append(K_About,_("&About\tF1"),_("About this program"));
+
+	// create top menu bar
+	wxMenuBar* menuBar = new wxMenuBar();
+	menuBar->Append(helpMenu,_("Help"));
+
+	// put the menubar on this frame
+	this->SetMenuBar(menuBar);
+
+	SetStatusText(_("Kinect3DBuilder ready!"));
+	panel = new wxPanel(this);
+	
+	// buttons
+	wxButton* load_button = new wxButton(panel,wxID_OPEN,_("Load Image from File"),wxDefaultPosition,wxDefaultSize,wxBU_EXACTFIT,
+										wxDefaultValidator,_("Load previously saved PNG image along with the depth file"));
+	wxButton* connectk_button = new wxButton(panel,wxID_ANY,_("Start Kinect Device"),wxDefaultPosition,wxDefaultSize,
+											wxBU_EXACTFIT,wxDefaultValidator);
+	
+	// put them on a sizer
+	wxSizer* sizer = new wxBoxSizer(wxHORIZONTAL);
+	sizer->Add(load_button,2);
+	sizer->Add(connectk_button,2);
+
+	panel->SetSizer(sizer);
+	//SetBackgroundColour(wxColour(255,255,255));
+	
+	// connect buttons to functionality
+	//load_button->Connect(
+
+}
+
+MainFrame::~MainFrame()
+{
 }
 
 bool MainFrame::OnInit()
 {
 	return true;
+}
+
+void MainFrame::OnAbout(wxCommandEvent& WXUNUSED(event))
+{
+	 //TODO: complete this
 }
