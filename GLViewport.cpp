fc18d01b274cc70c472bb2b3a47152212e85c519
henriquenj@gmail.com <henriquenj@gmail.com@fb52f868-46a9-6944-04f2-c40b472b6950>
2013-08-10 21:46:39 +0000

    * implement loop using a timer

--- GLViewport.cpp
+++ GLViewport.cpp
@@ -7,21 +7,25 @@ GLViewport::GLViewport(wxWindow *parent, wxWindowID id,const wxPoint& pos, const
 	// create context
 	m_context = new wxGLContext(this);
 	this->Connect(wxID_ANY,wxEVT_PAINT,wxPaintEventHandler(GLViewport::OnPaint));
-	// TODO: PREVENT SUPER LOOP
+
+	// we can assume NO_LOOP just checkin if this is null
+	timer = NULL;
 }
 
 
 GLViewport::~GLViewport(void)
 {
 	delete m_context;
+	timer->Stop();
+	delete timer;
 }
 
 void GLViewport::OnPaint(wxPaintEvent& WXUNUSED(event))
 {
 
-	// somehow this solve the problem with the render loop
+	// somehow this solve the problem with the render loop (ask the wx guys for more info)
 	wxPaintDC dc(this);
-
+	
 	// set the current OpenGL state machine
 	this->SetCurrent();
 
@@ -38,4 +42,44 @@ void GLViewport::OnPaint(wxPaintEvent& WXUNUSED(event))
 	glFlush();
 
 	this->SwapBuffers();
+}
+
+void GLViewport::ChangeLoopMode(bool loopMode)
+{
+	// change loop mode based on parameter
+	if (!loopMode)
+	{
+		if (timer != NULL)
+		{
+			delete timer;
+			timer = NULL;
+		}
+	}
+	else if (loopMode)
+	{
+		// create timer
+		timer = new RenderTimer(this);
+		timer->Start();
+	}
+}
+
+
+
+/* RENDERTIMER class */
+
+RenderTimer::RenderTimer(GLViewport* viewport)
+{
+	// copy pointer to call drawn inside notify
+	this->viewport = viewport;
+}
+
+void RenderTimer::Start()
+{
+	wxTimer::Start(10);
+}
+
+void RenderTimer::Notify()
+{
+	// update OpenGL view
+	viewport->Refresh(false);
 }
