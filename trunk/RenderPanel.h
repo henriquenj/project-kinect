f0e97af94513b2a981298ca0467832d8a2cb46a5
henriquenj@gmail.com <henriquenj@gmail.com@fb52f868-46a9-6944-04f2-c40b472b6950>
2013-09-03 20:32:36 +0000

    * Added interface to connect with the Kinect device

---
+++ trunk/RenderPanel.h
@@ -0,0 +1,54 @@
+#ifndef __RENDERPANEL_CLASS__
+#define __RENDERPANEL_CLASS__
+
+
+#include "wx\wx.h"
+#include "AppConfig.h"
+
+class RenderTimer;
+
+// class that'll control the render loop separeted from the interface
+class RenderPanel : public wxPanel
+{
+public:
+	RenderPanel(wxWindow *parent, wxWindowID id = wxID_ANY,
+		const wxPoint& pos = wxDefaultPosition,
+		const wxSize& size = wxDefaultSize);
+
+	~RenderPanel(void);
+
+	/* change loop mode: true for looping and false for non-loopig
+		the default is FALSE*/
+	void ChangeLoopMode(bool loopMode);
+
+	// callback function
+	// on paint is called at each frame
+	void OnPaint(wxPaintEvent &event);
+
+private:
+	// our timer
+	RenderTimer* timer;
+
+	// static bitmap will render the loaded image on the screen
+	wxStaticBitmap* rgbBitmap;
+	wxStaticBitmap* depthBitmap;
+
+	friend class MainPanel; // make communication easier
+};
+
+
+/*RenderTimer class implements the timer which will control the render loop */
+class RenderTimer : public wxTimer
+{
+public:
+	RenderTimer(RenderPanel* viewport);
+	//start the timer with interval of 10 miliseconds
+	void Start();
+	// functiona callback to becalled upon need to redraw
+	void Notify();
+private:
+	RenderPanel* viewport;
+};
+
+
+#endif //__RENDERPANEL_CLASS__
